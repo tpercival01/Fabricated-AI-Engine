2af1e01d5545c06357317376eeff2c8ae005d6d7
Thomas Percival <thomaspercival@live.co.uk>
2026-03-20 17:40:55 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,12 +2,23 @@ import copy
 
 def sanitize_case_for_frontend(case_data: dict) -> dict:
     case_data_copy = copy.deepcopy(case_data)
-    case_data_copy.pop('core_truth', None)
     case_data_copy.pop('solution_explanation', None)
 
-    for suspect in case_data_copy["suspects"]:
-        suspect.pop('is_guilty', None)
-        suspect.pop('alibi_flaw', None)
+    if "core_truth" in case_data_copy:
+        case_data_copy["core_truth"].pop("killer_name", None)
+        case_data_copy["core_truth"].pop("killer_motive", None)
+        case_data_copy["core_truth"].pop("killer_alibi", None)
+        case_data_copy["core_truth"].pop("killer_alibi_flaw", None)
     
+    if "suspects" in case_data_copy:
+        for suspect in case_data_copy["suspects"]:
+            suspect.pop("is_guilty", None)
+            suspect.pop("alibi_flaw", None)
+            
+    if "clues" in case_data_copy:
+        for clue in case_data_copy["clues"]:
+            clue.pop("points_to_suspect_id", None)
+            clue.pop("deduction_logic", None)
+            
     return case_data_copy
     
